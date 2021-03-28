9763f2257955e9b804fc8ab5dc66125f63801aa5
MonicaKang <mokaex@gmail.com>
2021-03-28 12:23:34 -0700

    Adding all files

---
+++ Twint/config.py
@@ -0,0 +1,4 @@
+access_key = "1355257540615913474-Uew31a4GMEDapwPH7Ttqxf0W1bjmXo"
+access_secret = "sJ6mVpNmQGcUXZAqpkMrE6TQpJGdiZzxopBESzJDNGCLA"
+consumer_key = "kEDyhzGwdiSWQoY9HXE37wvie"
+consumer_secret = "RpTmOVKMC77mBdUbyEnSacIRGX1gycPN7nrf5pDke6FWiw1u9d"
